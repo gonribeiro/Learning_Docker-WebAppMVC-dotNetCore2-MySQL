1d2e16c926c8578d6d4fd084215f0e465bc195f1
Gon <gon@gon>
2019-09-15 09:52:19 -0300

    CRUD em funcionamentoe e sintaxe docker correta para uso

---
+++ README.md
@@ -0,0 +1,2 @@
+- Link do projeto no DockerHub:
+https://hub.docker.com/r/tiagoribeirog/webapp-mvc_dotnet-core2_mysql
---
+++ webapp/Models/ProductContext/ProductContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace webapp.Models.ProductContext
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            // Conexão compilando apenas o MySQL via docker
+            // optionsBuilder.UseMySQL("server=localhost;port=3333;userid=root;password=123456;database=amazingdb;");
+
+            // Conexão compilando a aplicação .NET e MySQL juntas via docker
+            optionsBuilder.UseMySQL("server=db;port=3306;userid=root;password=123456;database=amazingdb;");
+        }
+
+        public ProductContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
