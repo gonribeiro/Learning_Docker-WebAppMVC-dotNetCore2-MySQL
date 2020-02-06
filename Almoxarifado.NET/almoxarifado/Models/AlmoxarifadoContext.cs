536f8148e4d25b90bad84cb9fdf5d9aa630b42b2
Gon <gon@gon>
2020-02-06 20:39:58 -0300

    Adicionado almoxarife

---
+++ Almoxarifado.NET/almoxarifado/Models/AlmoxarifadoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using almoxarifado.Models;
+
+namespace almoxarifado.Models
+{
+    public class AlmoxarifadoContext : DbContext
+    {
+        public AlmoxarifadoContext(DbContextOptions<AlmoxarifadoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ Almoxarifado.NET/almoxarifado/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace almoxarifado.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Fabricante { get; set; }
+        public string CodigoBarras { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
